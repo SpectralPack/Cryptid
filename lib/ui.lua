0c3fd56153c44463bbb909b0926849aa77ed0a61
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-06-03 17:17:07 +0100

    fix floating sprite related shenanigans

--- lib/ui.lua
+++ lib/ui.lua
@@ -200,28 +200,45 @@ SMODS.DrawStep({
 		then
 			local scale_mod = 0.07 -- + 0.02*math.cos(1.8*G.TIMERS.REAL) + 0.00*math.cos((G.TIMERS.REAL - math.floor(G.TIMERS.REAL))*math.pi*14)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^3
 			local rotate_mod = 0 --0.05*math.cos(1.219*G.TIMERS.REAL) + 0.00*math.cos((G.TIMERS.REAL)*math.pi*5)*(1 - (G.TIMERS.REAL - math.floor(G.TIMERS.REAL)))^2
-			self.children.floating_sprite2:draw_shader(
-				"dissolve",
-				0,
-				nil,
-				nil,
-				self.children.center,
-				scale_mod,
-				rotate_mod,
-				nil,
-				0.1 --[[ + 0.03*math.cos(1.8*G.TIMERS.REAL)--]],
-				nil,
-				0.6
-			)
-			self.children.floating_sprite2:draw_shader(
-				"dissolve",
-				nil,
-				nil,
-				nil,
-				self.children.center,
-				scale_mod,
-				rotate_mod
-			)
+			if self.children.floating_sprite2 then
+				self.children.floating_sprite2:draw_shader(
+					"dissolve",
+					0,
+					nil,
+					nil,
+					self.children.center,
+					scale_mod,
+					rotate_mod,
+					nil,
+					0.1 --[[ + 0.03*math.cos(1.8*G.TIMERS.REAL)--]],
+					nil,
+					0.6
+				)
+				self.children.floating_sprite2:draw_shader(
+					"dissolve",
+					nil,
+					nil,
+					nil,
+					self.children.center,
+					scale_mod,
+					rotate_mod
+				)
+			else
+				local center = self.config.center
+				if _center and _center.soul_pos and _center.soul_pos.extra then
+					self.children.floating_sprite2 = Sprite(
+						self.T.x,
+						self.T.y,
+						self.T.w,
+						self.T.h,
+						G.ASSET_ATLAS[_center.atlas or _center.set],
+						_center.soul_pos.extra
+					)
+					self.children.floating_sprite2.role.draw_major = self
+					self.children.floating_sprite2.states.hover.can = false
+					self.children.floating_sprite2.states.click.can = false
+				end
+			end
 		end
 	end,
 	conditions = { vortex = false, facing = "front" },
