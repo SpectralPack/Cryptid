681a058d2b7612e00237f8fd8e76c70961c2491a
Jevonnissocoolman <jevonnsivels2@gmail.com>
2025-06-07 12:26:54 -0700

    Fix Locked description

--- items/misc_joker.lua
+++ items/misc_joker.lua
@@ -5936,6 +5936,13 @@ local annihalation = {
 			},
 		}
 	end,
+	locked_loc_vars = function(self, info_queue, card)
+		return {
+			vars = {
+				localize(card.ability.extra.type, "poker_hands"),
+			},
+		}
+	end,
 	atlas = "atlasthree",
 	rarity = 3,
 	cost = 8,
--- localization/en-us.lua
+++ localization/en-us.lua
@@ -1440,7 +1440,7 @@ return {
 			j_cry_crustulum = {
 				name = "Crustulum",
 				text = {
-					"This Joker gains {C:chips}+#2#{} Chip#<s>2#",
+					"This Joker gains {C:chips}+#2#{} Chip#<s>1#",
 					"per {C:attention}reroll{} in the shop",
 					"{C:green}All rerolls are free{}",
 					"{C:inactive}(Currently {C:chips}+#1#{C:inactive} chip#<s>1#)",
@@ -2843,12 +2843,17 @@ return {
 					"hand contains",
 					"a {C:attention}#2#",
 				},
+				unlock = {
+					"Win a run with",
+					"your final hand being",
+					"a {E:1,C:attention}#1#",
+				},
 			},
 			j_cry_trick_or_treat = {
 				name = "Trick-or-Treat",
 				text = {
 					"When {C:attention}sold{}:",
-					"{C:green}#1# in #2#{} chance to create {C:attention}#3#{} {C:cry_candy}Candies",
+					"{C:green}#1# in #2#{} chance to create {C:attention}#3{} {C:cry_candy}Candies",
 					"Otherwise, create a {X:cry_cursed,C:white}Cursed{} Joker",
 					"{C:inactive}(Can overflow)",
 				},
