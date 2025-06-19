916d30244ee78940ae353b629d7ba2be25a079b7
Ruby <darkdragongaming379@gmail.com>
2025-06-19 13:27:11 +0100

    bignum blacklist negative joker

--- lib/misc.lua
+++ lib/misc.lua
@@ -522,6 +522,7 @@ Cryptid.big_num_blacklist = {
 	["j_cry_fractal"] = true,
 	["j_cry_wonka_bar"] = true,
 	["j_cry_oldcandy"] = true,
+	["j_cry_negative"] = true,
 
 	["c_magician"] = true,
 	["c_empress"] = true,
