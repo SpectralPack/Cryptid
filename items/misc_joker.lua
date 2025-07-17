49883a36db126d1c83770850720c22d07b183621
Jevonnissocoolman <jevonnsivels2@gmail.com>
2025-07-17 14:53:53 -0700

    Fix Pirate dagger Incorrectly checking eternal

--- items/misc_joker.lua
+++ items/misc_joker.lua
@@ -3132,7 +3132,7 @@ local pirate_dagger = {
 			and not (context.blueprint_card or self).getting_sliced
 			and my_pos
 			and G.jokers.cards[my_pos + 1]
-			and not SMODS.is_eternal(G.jokers.cards[my_pos - 1])
+			and not SMODS.is_eternal(G.jokers.cards[my_pos + 1])
 			and not G.jokers.cards[my_pos + 1].getting_sliced
 		then
 			local sliced_card = G.jokers.cards[my_pos + 1]
