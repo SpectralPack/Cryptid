681671e928507001569d11f84412466ebc03a3c8
sweshelo <sweshelo@gmail.com>
2025-11-05 00:48:27 +0900

    pass target filepath as arg

--- localization/tools/find_missing_adv.py
+++ localization/tools/find_missing_adv.py
@@ -7,6 +7,9 @@ stuff like missing unlock descriptors.
 Author: Avery (@onichama)
 """
 
+import sys
+import os
+
 vars_en = []
 vars_other = []
 
@@ -21,8 +24,19 @@ def read_vars_from_file(filename, into_list):
             if ("},\n" in line or "}\n" in line) and len(current_var_path) > 0:
                 current_var_path.pop(len(current_var_path)-1)
 
+# Get target language file from command line argument or prompt
+if len(sys.argv) > 1:
+    target_file = sys.argv[1]
+else:
+    target_file = input("Enter the path to the language file to compare (e.g., ../de.lua): ")
+
+# Check if the target file exists
+if not os.path.exists(target_file):
+    print(f"Error: File '{target_file}' not found")
+    sys.exit(1)
+
 read_vars_from_file("../en-us.lua", vars_en) # Take EN-US as base language
-read_vars_from_file("../de.lua", vars_other) # Change this to the language you want to compare to
+read_vars_from_file(target_file, vars_other) # Compare to specified language file
 
 for var in vars_en:
     if var not in vars_other:
