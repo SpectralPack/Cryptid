ab29992a5a4bb886cb7f18bac377046e05f5c7c8
paulkent-coder <paulkent-041@gmail.com>
2026-05-01 08:56:52 -0400

    Fixed Sol's Observatory effect

--- lib/ascended.lua
+++ lib/ascended.lua
@@ -246,11 +246,11 @@ function Cryptid.calculate_ascension_power(hand_name, hand_cards, hand_scoring_c
 	-- Get Ascension Power From Sol/Perkele (Observatory effect)
 	if
 		G.GAME.used_vouchers.v_observatory
-		and (next(SMODS.find_card("cry-sunplanet")) or next(SMODS.find_card("cry-Perkele")))
+		and (next(find_joker("cry-sunplanet")) or next(find_joker("cry-Perkele")))
 	then
 		-- switch this to not use find_joker eventually please for the love of god
-		local super_entropic_local_variable_that_stores_the_amount_of_suns = #SMODS.find_card("cry-sunplanet")
-			+ #SMODS.find_card("cry-Perkele")
+		local super_entropic_local_variable_that_stores_the_amount_of_suns = #find_joker("cry-sunplanet")
+			+ #find_joker("cry-Perkele")
 
 		if super_entropic_local_variable_that_stores_the_amount_of_suns == 1 then
 			bonus = bonus + 1
