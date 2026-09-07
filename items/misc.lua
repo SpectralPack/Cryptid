47a95173fcc250c77ee1a106941f0910053f4e57
Eris (borb) <remcosveen@gmail.com>
2026-09-07 09:04:52 +0200

    fix astral code being weird

--- items/misc.lua
+++ items/misc.lua
@@ -1204,21 +1204,13 @@ local astral = {
 		if
 			(
 				context.edition -- for when on jonklers
-				and context.cardarea == G.jokers -- checks if should trigger
-				and card.config.trigger -- fixes double trigger
+				and context.post_joker
 			) or (
 				context.main_scoring -- for when on playing cards
 				and context.cardarea == G.play
 			)
 		then
 			return { e_mult = card and card.edition and card.edition.e_mult or self.config.e_mult } -- updated value
-		end
-		if context.joker_main then
-			card.config.trigger = true -- context.edition triggers twice, this makes it only trigger once (only for jonklers)
-		end
-
-		if context.after then
-			card.config.trigger = nil
 		end
 	end,
 	attributes = { "emult" },
