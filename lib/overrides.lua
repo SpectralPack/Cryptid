db808898f17428bba6b98438dcf1d5bbec80c5c4
Eris (borb) <remcosveen@gmail.com>
2026-09-23 18:19:19 +0200

    remove slib gameset check

--- lib/overrides.lua
+++ lib/overrides.lua
@@ -5,7 +5,7 @@
 -- essentially in_pool for equilibrium spawning
 function Cryptid.can_spawn_equilibrium(center)
 	if
-		(Cryptid.enabled(center.key) ~= true or (Spectrallib and Spectrallib.enabled(center.key) ~= true))
+		(Cryptid.enabled(center.key) ~= true)
 		and center.original_mod
 	then
 		return false
